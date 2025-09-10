06df6bb3d00576f2235926acf2a4221fc2a780fa
Nahid Hasan <nahidhasan9613@gmail.com>
2025-09-10 09:38:59 +0600

    managing API keys and settings

---
+++ app/config.py
@@ -0,0 +1,14 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+class Settings(BaseSettings):
+    """
+    Application settings loaded from environment variables.
+    """
+    VAPI_API_KEY: str
+    TWILIO_ACCOUNT_SID: str
+    TWILIO_AUTH_TOKEN: str
+    BACKEND_API_BASE_URL: str
+
+    model_config = SettingsConfigDict(env_file=".env", extra='ignore')
+
+settings = Settings()
